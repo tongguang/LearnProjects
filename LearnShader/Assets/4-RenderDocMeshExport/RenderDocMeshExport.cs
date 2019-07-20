106c3fc02ca5d224a42d708883522217a73dfcb0
tongguang <591912867@qq.com>
2019-07-21 02:41:13 +0800

    docmesh

--- LearnShader/Assets/4-RenderDocMeshExport/RenderDocMeshExport.cs
+++ LearnShader/Assets/4-RenderDocMeshExport/RenderDocMeshExport.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
 public class RenderDocMeshExport
 {
     public class RenderDocMesh
